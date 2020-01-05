9e531b734b06a1608e500308779fe8215cf23f57
Ronald Escobar <ronaldescobarj@gmail.com>
2020-01-05 00:38:08 -0400

    sending notifications

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ admin.initializeApp({
     databaseURL: "https://final-iot-3059e.firebaseio.com"
 });
 
-var registrationToken = "<registration token goes here>";
+var registrationToken = "dbeRQk-3EUM:APA91bHVIo_cLjaO8GRqqI-xFbeshrhqmhYINuVyoZsuqw4Ke00JwxDyWEEam6OhVaju66Y-r4kgyJM-HW4kT6o00DwGThwK7fJdGZNiS8pggzm6ZOvnyUvmHXtMcug8XPuvoqEoRuaj";
 
 var payload = {
     notification: {
@@ -23,16 +23,17 @@ var options = {
     timeToLive: 60 * 60 * 24
 };
 
-// admin.messaging().sendToDevice(registrationToken, payload, options)
-//     .then(function (response) {
-//         console.log("Successfully sent message:", response);
-//     })
-//     .catch(function (error) {
-//         console.log("Error sending message:", error);
-//     });
+
 
 app.post('/timbre', function (req, res) {
-    admin.messaging().send(payload)
+    // admin.messaging().send(payload)
+    //     .then(function (response) {
+    //         console.log("Successfully sent message:", response);
+    //     })
+    //     .catch(function (error) {
+    //         console.log("Error sending message:", error);
+    //     });
+    admin.messaging().sendToDevice(registrationToken, payload, options)
         .then(function (response) {
             console.log("Successfully sent message:", response);
         })
